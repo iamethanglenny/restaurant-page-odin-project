6d620a4669aa2c73cf5cbe6278a5ff669edf28a1
iamethanglenny <ethanglenny@gmail.com>
2024-10-24 18:57:34 +0200

    feat: Add eventlisteners to buttons to change tabs between home, menu, and about. issue: Hide current content when button is clicked.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import { loadContent } from './loadContent.js';
+import { loadMenuContent } from './menu-content.js';
+import { loadAboutContent } from './about-content.js';
 import "./styles.css";
 
 document.addEventListener('DOMContentLoaded', () => {
     loadContent();
 });
 
+
+const homeBtn = document.getElementById("home");
+const menuBtn = document.getElementById("menu");
+const aboutBtn = document.getElementById("about");
+
+homeBtn.addEventListener("click", () => {
+    loadContent();
+});
+
+menuBtn.addEventListener("click", () => {
+    loadMenuContent();
+});
+
+aboutBtn.addEventListener("click", () => {
+    loadAboutContent();
+});
