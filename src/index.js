8e92e479a3d5ef65b1002f1604c459fbcac23b2e
iamethanglenny <ethanglenny@gmail.com>
2024-10-25 06:26:20 +0200

    feat: Add clearContent function to clear previous content on the page when clicking a new tab

--- src/index.js
+++ src/index.js
@@ -7,19 +7,27 @@ document.addEventListener('DOMContentLoaded', () => {
     loadContent();
 });
 
+const contentDiv = document.getElementById("content");
+
+function clearContent() {
+    contentDiv.innerHTML = "";
+}
 
 const homeBtn = document.getElementById("home");
 const menuBtn = document.getElementById("menu");
 const aboutBtn = document.getElementById("about");
 
 homeBtn.addEventListener("click", () => {
+    clearContent();
     loadContent();
 });
 
 menuBtn.addEventListener("click", () => {
+    clearContent();
     loadMenuContent();
 });
 
 aboutBtn.addEventListener("click", () => {
+    clearContent();
     loadAboutContent();
 });
